3611b13e34c6085dbcf94751742b802b978f0a61
Ines Trabelsi <ines.jenfaoui@gmail.com>
2025-05-26 19:00:53 +0200

    code refactoring

--- scripts/factories/RecipeFactory.js
+++ scripts/factories/RecipeFactory.js
@@ -29,46 +29,59 @@ export class RecipeFactory {
     };
   }
 
-  filterRecipes(recipes, filterChoices) {
+  generalSearch(recipe, filterChoices) {
     const general = filterChoices.general?.toLowerCase() || "";
-    const selectedIngredients = filterChoices.ingredients || [];
-    const selectedAppliances = filterChoices.appliances || [];
-    const selectedUstensils = filterChoices.ustensils || [];
-
-    return recipes.filter((recipe) => {
-      // Recherche générale : nom, déscription ou ingrédient
-      const matchesGeneral =
-        !general ||
+    return !general ||
         recipe.name.toLowerCase().includes(general) ||
         recipe.description.toLowerCase().includes(general) ||
         recipe.ingredients.some((ing) =>
           ing.ingredient.toLowerCase().includes(general)
         );
+  }
 
-      // Filtre des ingrédients: tous les ingrédients sélectionnés doivent être présents
-      const matchesIngredients =
-        selectedIngredients.length === 0 ||
-        selectedIngredients.every((ingredientName) =>
-          recipe.ingredients.some(     
-            (ing) => ing.ingredient.toLowerCase() === ingredientName
-          )
-        );
+  criteriaSearch(recipe, filterChoices) {
+     const selectedIngredients = filterChoices.ingredients || [];
+    const selectedAppliances = filterChoices.appliances || [];
+    const selectedUstensils = filterChoices.ustensils || [];
 
-      // Filtre des appareils : tous les appareils doivent être présents
-      const matchesAppliances =
-        selectedAppliances.length === 0 ||
-        selectedAppliances.every(
-          (applianceName) => recipe.appliance.toLowerCase() === applianceName
-        );
+    // Filtre des ingrédients: tous les ingrédients sélectionnés doivent être présents
+    const matchesIngredients =
+      selectedIngredients.length === 0 ||
+      selectedIngredients.every((ingredientName) =>
+        recipe.ingredients.some(     
+          (ing) => ing.ingredient.toLowerCase() === ingredientName
+        )
+      );
 
-      // Filtre des ustensils : tous les ustensils doivent être présents
-      const matchesUstensils =
-        selectedUstensils.length === 0 ||
-        selectedUstensils.every((ustensilName) =>
-          recipe.ustensils.some(
-            (ustensil) => ustensil.toLowerCase() === ustensilName
-          )
-        );
+    // Filtre des appareils : tous les appareils doivent être présents
+    const matchesAppliances =
+      selectedAppliances.length === 0 ||
+      selectedAppliances.every(
+        (applianceName) => recipe.appliance.toLowerCase() === applianceName
+      );
+
+    // Filtre des ustensils : tous les ustensils doivent être présents
+    const matchesUstensils =
+      selectedUstensils.length === 0 ||
+      selectedUstensils.every((ustensilName) =>
+        recipe.ustensils.some(
+          (ustensil) => ustensil.toLowerCase() === ustensilName
+        )
+      );
+    
+    return {
+      matchesIngredients,
+      matchesAppliances,
+      matchesUstensils,
+    }
+  }
+
+  filterRecipes(recipes, filterChoices) {
+    return recipes.filter((recipe) => {
+      // Recherche générale : nom, déscription ou ingrédient
+      const matchesGeneral = this.generalSearch(recipe, filterChoices);
+
+      const {matchesIngredients, matchesAppliances, matchesUstensils} = this.criteriaSearch(recipe, filterChoices);      
 
       return (
         matchesGeneral &&
